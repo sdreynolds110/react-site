d2580454ae4d6a07725d19661aab7f4b46537047
Stephen Reynolds <sdreynolds110@gmail.com>
2021-10-13 13:44:21 -0400

    initial commit, with some routing completed, without transitions

---
+++ src/components/common/Header.js
@@ -0,0 +1,18 @@
+import React, {Component} from 'react';
+import Link from 'react-router-dom';
+
+class Header extends Component {
+    render() {
+        return (
+            <header className="masthead">
+                <div className="container">
+                    <div className="masthead-subheading">{this.props.title}</div>
+                    <div className="masthead-heading text-uppercase">{this.props.subtitle}</div>
+                    <Link className="btn btn-primary btn-xl text-uppercase" to={this.props.link}>{this.props.buttonText}</Link>
+                </div>
+            </header>
+        )
+    }
+}
+
+export default Header;
